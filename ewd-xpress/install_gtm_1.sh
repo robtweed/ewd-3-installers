79eadbc39cbb32f8dede750dacb46b10b5a89158
robtweed <rtweed@mgateway.com>
2016-05-10 12:22:39 +0100

    Added acknowledgement

--- ewd-xpress/install_gtm_1.sh
+++ ewd-xpress/install_gtm_1.sh
@@ -3,6 +3,9 @@
 # run using: source install_gtm_1.sh
 # Must be root!
 
+# Acknowledgements: Chris Edwards for the code below that checks 
+$                   if running as root
+
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root" 1>&2
     exit 1
