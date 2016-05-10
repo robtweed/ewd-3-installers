466efab5605837fd8289c4f7e4cd5fd02757fd8d
robtweed <rtweed@mgateway.com>
2016-05-10 12:20:17 +0100

    Added GT.M installers

---
+++ ewd-xpress/install_gtm_1.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# run using: source install_gtm_1.sh
+# Must be root!
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+echo 'Installing GT.M'
+
+wget https://sourceforge.net/projects/fis-gtm/files/GT.M%20Installer/v0.13/gtminstall/download -O gtminstall.sh
+source gtminstall.sh
+
+echo 'Done!'
+echo 'Now switch back to normal user and run: source install_gtm_2.sh'
+
+
---
+++ ewd-xpress/install_gtm_2.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+# run using: source install_gtm_2.sh
+
+# run as normal user, eg ubuntu
+
+
+# Prepare
+
+echo 'Preparing environment'
+
+sudo apt-get update
+sudo apt-get install -y build-essential
+sudo apt-get install -y wget gzip openssh-server curl
+
+# GT.M
+
+echo 'Configuring GT.M'
+
+source "/usr/lib/fis-gtm/V6.3-000_x86_64"/gtmprofile
+echo 'source "/usr/lib/fis-gtm/V6.3-000_x86_64"/gtmprofile' >> ~/.profile
+
+# Node.js
+
+echo 'Installing Node.js'
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+source ~/.nvm/nvm.sh
+nvm alias default 4.2
+nvm install 4.2
+nvm use default
+echo 'nvm use default' >> ~/.profile
+
+sudo ln -s /usr/local/bin/node /usr/bin/node
+sudo ln -s /usr/local/lib/node /usr/lib/node
+sudo ln -s /usr/local/bin/npm /usr/bin/npm
+sudo ln -s /usr/local/bin/node-waf /usr/bin/node-waf
+n=$(which node);n=${n%/bin/node}; chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local
+
+# ewd-xpress
+
+echo 'Installing ewd-qoper8 and associated modules'
+
+cd ~
+mkdir ewd
+cd ewd
+npm install express body-parser ewd-qoper8 ewd-qoper8-express ewd-session ewd-qoper8-cache ewd-qoper8-gtm socket.io
+npm install ewd-xpress ewd-xpress-monitor
+
+# NodeM
+
+echo 'Installing NodeM'
+
+npm install nodem
+sudo ln -sf $gtm_dist/libgtmshr.so /usr/local/lib/
+sudo ldconfig
+base=~/ewd
+[ -f "$GTMCI" ] || export GTMCI="$(find $base -iname nodem.ci)"
+nodemgtmr="$(find $base -iname v4wnode.m | tail -n1 | xargs dirname)"
+echo "$gtmroutines" | fgrep "$nodemgtmr" || export gtmroutines="$nodemgtmr $gtmroutines"
+
+echo 'base=~/ewd'
+echo '[ -f "$GTMCI" ] || export GTMCI="$(find $base -iname nodem.ci)"' >> ~/.profile
+echo 'nodemgtmr="$(find $base -iname v4wnode.m | tail -n1 | xargs dirname)"' >> ~/.profile
+echo 'echo "$gtmroutines" | fgrep "$nodemgtmr" || export gtmroutines="$nodemgtmr $gtmroutines"' >> ~/.profile
+
+# ewd-express
+
+echo 'Moving ewd-express files into place'
+
+mv ~/ewd/node_modules/ewd-xpress/example/ewd-xpress.js ~/ewd/ewd-xpress.js
+
+cd ~/ewd
+mkdir www
+cd www
+mkdir ewd-xpress-monitor
+cp ~/ewd/node_modules/ewd-xpress-monitor/www/bundle.js ~/ewd/www/ewd-xpress-monitor
+cp ~/ewd/node_modules/ewd-xpress-monitor/www/*.html ~/ewd/www/ewd-xpress-monitor
+cp ~/ewd/node_modules/ewd-xpress-monitor/www/*.css ~/ewd/www/ewd-xpress-monitor
+
+cd ~/ewd
+
+echo 'Done!'
+
