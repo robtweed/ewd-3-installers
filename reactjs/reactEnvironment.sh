ba79f09634787302a4781f2dd0b35ee087dc6734
robtweed <rtweed@mgateway.com>
2016-05-11 11:11:44 +0100

    Added uglify to React environment

--- reactjs/reactEnvironment.sh
+++ reactjs/reactEnvironment.sh
@@ -8,11 +8,16 @@ npm install react@0.14 react-dom@0.14 babelify babel-preset-react react-bootstra
 npm install jquery ewd-client ewd-react-tools ewd-xpress-react
 
 npm install -g browserify
+npm install -g uglify-js
 
 cd ~/ewd3/www/ewd-xpress-monitor
 npm install babel-preset-es2015
 
 # Now you can compile an application bundle:
+#  cp ~/ewd3/node_modules/ewd-xpress-monitor/www/*.js ~/ewd3/www/ewd-xpress-monitor
 #  cd ~/ewd3/www/ewd-xpress-monitor
-#  browserify -t [ babelify --presets [es2015 react] ] app.js -o bundle.js
+#  or
+#  cd ~/ewd3/node_modules/ewd-xpress-monitor/www
+#  browserify -t [ babelify --compact false --presets [es2015 react] ] app.js | uglifyjs > bundle.js
+#  cp ~/ewd3/node_modules/ewd-xpress-monitor/www/bundle.js ~/ewd3/www/ewd-xpress-monitor
 
