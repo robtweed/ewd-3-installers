d8f9e168ff6e9114e8820d95aee49f5d487de3aa
robtweed <rtweed@mgateway.com>
2016-05-19 13:57:00 +0100

    Amended example path

--- reactjs/createTestReactApp.sh
+++ reactjs/createTestReactApp.sh
@@ -12,19 +12,19 @@ mkdir example-socket-app
 
 # Copy in the HTML and CSS resources from the ewd-xpress-react module
 
-cp ~/ewd3/node_modules/ewd-xpress-react/example/*.html ~/ewd3/www/example-socket-app
-cp ~/ewd3/node_modules/ewd-xpress-react/example/*.css ~/ewd3/www/example-socket-app
+cp ~/ewd3/node_modules/ewd-xpress-react/example/reactjs/*.html ~/ewd3/www/example-socket-app
+cp ~/ewd3/node_modules/ewd-xpress-react/example/reactjs/*.css ~/ewd3/www/example-socket-app
 
 # Copy the back-end module to the node_modules directory
 
-cp ~/ewd3/node_modules/ewd-xpress-react/example/node_modules/example-socket-app.js ~/ewd3/node_modules
+cp ~/ewd3/node_modules/ewd-xpress-react/example/reactjs/node_modules/example-socket-app.js ~/ewd3/node_modules
 
 echo 'Done!'
 
 # Now you can compile an application bundle for this app:
-#  cd ~/ewd3/node_modules/ewd-xpress-react/example
+#  cd ~/ewd3/node_modules/ewd-xpress-react/example/reactjs
 #  browserify -t [ babelify --compact false --presets [es2015 react] ] app.js | uglifyjs > bundle.js
-#  cp ~/ewd3/node_modules/ewd-xpress-react/example/bundle.js ~/ewd3/www/example-socket-app
+#  cp ~/ewd3/node_modules/ewd-xpress-react/example/reactjs/bundle.js ~/ewd3/www/example-socket-app
 
 # Run it using http://192.168.1.100:8080/example-socket-app/index.html
 
