4ee3ad5b474d9b69f2413e45700fecd07a4c26af
robtweed <rtweed@mgateway.com>
2016-09-24 14:27:22 +0100

    GT.M installer updated with kind assistance from KS Bhaskar

--- ewd-xpress/install_gtm.sh
+++ ewd-xpress/install_gtm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# run using: source install_gtm_2.sh
+# run using: source install_gtm.sh
 
 # Acknowledgement: Wladimir Mutel for NodeM configuration logic
 #                  KS Bhaskar for GT.M installation logic
@@ -24,21 +24,52 @@ mkdir /tmp/tmp # Create a temporary directory for the installer
 cd /tmp/tmp    # and change to it. Next command is to download the GT.M installer
 wget https://sourceforge.net/projects/fis-gtm/files/GT.M%20Installer/v0.13/gtminstall
 chmod +x gtminstall # Make the file executable
-sudo -E ./gtminstall --utf8 default --verbose # download and install GT.M including UTF-8 mode
+
+# Thanks to KS Bhaskar for the following enhancement:
+
+##sudo -E ./gtminstall --utf8 default --verbose # download and install GT.M including UTF-8 mode
+
+gtmroot=/usr/lib/fis-gtm
+gtmcurrent=$gtmroot/current
+if [ -e "$gtmcurrent"] ; then
+  mv -v $gtmcurrent $gtmroot/previous_`date -u +%Y-%m-%d:%H:%M:%S`
+fi
+sudo mkdir -p $gtmcurrent # make sure directory exists for links to current GT.M
+sudo -E ./gtminstall --utf8 default --verbose --linkenv $gtmcurrent --linkexec $gtmcurrent # download and install GT.M including UTF-8 mode
+
 
 echo 'Configuring GT.M'
 
-source "/usr/lib/fis-gtm/V6.3-000_x86_64"/gtmprofile
-echo 'source "/usr/lib/fis-gtm/V6.3-000_x86_64"/gtmprofile' >> ~/.profile
+# source "/usr/lib/fis-gtm/V6.3-000_x86_64"/gtmprofile
+# echo 'source "/usr/lib/fis-gtm/V6.3-000_x86_64"/gtmprofile' >> ~/.profile
+
+gtmprof=$gtmcurrent/gtmprofile
+gtmprofcmd="source $gtmprof"
+$gtmprofcmd
+tmpfile=`mktemp`
+if [ `grep -v "$gtmprofcmd" ~/.profile | grep $gtmroot >$tmpfile`] ; then
+  echo "Warning: existing commands referencing $gtmroot in ~/.profile may interfere with setting up environment"
+  cat $tmpfile
+fi
+if [ `grep -v "$gtmprofcmd" ~/.profile` ] ; then echo "$gtmprofcmd" >> ~/.profile ; fi
+rm $tmpfile
+unset tmpfile gtmprofcmd gtmprof gtmcurrent gtmroot
+
+echo 'GT.M has been installed and configured, ready for use'
+echo 'Enter the GT.M shell by typing the command: gtm  Exit it by typing the command H'
+
+# --- End of KS Bhaskar enhancement
 
 # Node.js
 
 echo 'Installing Node.js'
 
+cd ~
+
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 source ~/.nvm/nvm.sh
-nvm alias default 4.4
-nvm install 4.4
+nvm alias default 4
+nvm install 4
 nvm use default
 echo 'nvm use default' >> ~/.profile
 
@@ -91,4 +122,4 @@ cp ~/ewd3/node_modules/ewd-xpress-monitor/www/*.css ~/ewd3/www/ewd-xpress-monito
 cd ~/ewd3
 
 echo 'Done!'
-
+echo 'You should now be able to start ewd-xpress by typing: node ewd-xpress'
